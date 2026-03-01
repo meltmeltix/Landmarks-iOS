e9f319b0acca6c2d20dbd0091ad177e9295e8a35
Alessio Cameroni <alessioc.developer@gmail.com>
2026-03-01 09:53:50 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Alessio on 01/03/26.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
